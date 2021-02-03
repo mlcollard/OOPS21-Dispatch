c12b9f4867f56ac123c60b1e3c81f6072b0354e4
Michael L. Collard <collard@uakron.edu>
2021-02-03 12:45:19 -0500

    Add initial file with single parameter, scalar value overloading

---
+++ overloading.cpp
@@ -0,0 +1,16 @@
+/*
+    overloading.cpp
+
+    Example for demo of name mangling in C++
+*/
+
+#include <vector>
+
+void action(int n) {}
+void action(double x) {}
+void action(char c) {}
+
+int main() {
+
+    return 0;
+}
