0edd566512cc7a94d376d62fcc1f3e2fa476ca3a
Michael L. Collard <collard@uakron.edu>
2021-02-03 22:25:03 -0500

    Add more overloaded function definitions to the overloading example

--- overloading.cpp
+++ overloading.cpp
@@ -10,6 +10,15 @@ void action(int n) {}
 void action(double x) {}
 void action(char c) {}
 
+void action(double& x) {}
+void action(double* x) {}
+
+void action(const double& x) {}
+void action(const double* x) {}
+
+void action(std::vector<int>&) {}
+void action(const std::vector<int>&) {}
+
 int main() {
 
     return 0;
